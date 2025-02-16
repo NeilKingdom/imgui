9b952355384ebee27c0115d41177d0b6085d3797
NeilKingdom <neil@neilkingdom.xyz>
2025-02-16 16:30:55 -0500

    minor tweaks

--- imgui.mk
+++ imgui.mk
@@ -10,8 +10,20 @@ CFLAGS := -Ofast -I$(INC_DIR)
 SRCS := $(wildcard $(SRC_DIR)/*.cpp) $(wildcard $(BKND_SRC_DIR)/*.cpp)
 OBJS := $(patsubst $(SRC_DIR)/%.cpp, $(OBJ_DIR)/%.o, $(SRCS))
 
+all: mkdirs $(BIN_DIR)/imgui.a
+
+mkdirs:
+	@if [ ! -d "obj" ]; then \
+		mkdir obj; \
+	fi; \
+	if [ ! -d "bin" ]; then \
+		mkdir bin; \
+	fi
+
 $(BIN_DIR)/imgui.a: $(OBJS)
 	ar rcs $@ $^
 
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp
 	$(CC) $(CFLAGS) -c $< -o $@
+
+.PHONY: all mkdirs
